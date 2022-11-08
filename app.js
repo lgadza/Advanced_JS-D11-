aa0649badae8d852218c6c88daf6a1f114629b17
Louis Gadza <siuolgadza@gmail.com>
2022-11-08 13:09:19 +0100

    updates

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const options = {
 const divRow = document.querySelector(".row");
 const container = document.querySelector(".container");
 container.innerHTML = `<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>`;
+const containerFluid = document.querySelector("container-fluid");
 
 fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=pink%20floyd", options)
   .then((response) => response.json())
@@ -24,9 +25,9 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=pink%20floyd", options)
     container.innerHTML = "";
 
     for (i of listOfItems.data) {
-      container.innerHTML += `<div class="card col-12 col-sm-6 col-lg-4 col-xl-3 my-3" >
-                                    <img src=${i.album.cover} class="card-img-top" alt="...">
-                                    <div class="card-body">
+      container.innerHTML += `<div class="card-deck col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-3 c" >
+    <img src=${i.album.cover} class="card-img-top card" alt="...">
+                                    <div class="card-body mt-4">
                                       <h5 class="card-title">${i.album.title}</h5>                               
                                     </div>
                                   </div>`;
@@ -36,13 +37,14 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=pink%20floyd", options)
     () =>
       (container.innerHTML = `<div class="alert alert-danger">Something went wrong in getting items!</div>`)
   );
+container.innerHTML = `<img src=${listOfItems[1].album.cover} alt="jumbotron">`;
 //   .catch((err) => console.error(err));
 
 //fetch 2
 fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=daft%20punk", options)
   .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+  .then((response) => console.log(response));
+// .catch((err) => console.error(err));
 //fetch 3
 fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica", options)
   .then((response) => response.json())
